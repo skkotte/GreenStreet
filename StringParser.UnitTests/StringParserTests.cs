01049511425171caac9fc5078f7c1ced8582f2f9
skkotte <40211281+skkotte@users.noreply.github.com>
2024-06-05 20:15:53 +0100

    Add files via upload

---
+++ StringParser.UnitTests/StringParserTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace StringParser.UnitTests;
+
+[TestFixture]
+public class StringParserTests
+{
+    [Test]
+    public void Test()
+    {
+        1.ShouldBe(1);
+    }
+}
